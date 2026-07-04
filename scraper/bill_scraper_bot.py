ba113210e5aa717c0d6835379b064df7ef34866d
Mr. Rocket <manzoorahmedshaikh234@gmail.com>
2026-07-04 14:46:49 +0500

    Final fixes

--- scraper/bill_scraper_bot.py
+++ scraper/bill_scraper_bot.py
@@ -5,9 +5,9 @@ from .status_codes import *
 def get_hesco_bill(params : dict):
     session = cffi_requests.Session(impersonate="chrome")
     try:
-	    print(f"Checking bill for: {params}")
+        print(f"Checking bill for: {params}")
         session.get("https://bill.pitc.com.pk/hescobill/general", timeout=25)
-	    print("Session retrieved")
+        print("Session retrieved")
         response = session.post(
             "https://bill.pitc.com.pk/hescobill/general",
             params=params,
